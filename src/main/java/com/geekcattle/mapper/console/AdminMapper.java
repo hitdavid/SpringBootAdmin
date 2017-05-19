5f0639f45bb3d094066a30a506b302b36e9b7130
杜宇 <duyub@chanjet.com>
2017-05-19 15:24:41 +0800

    update

--- src/main/java/com/geekcattle/mapper/console/AdminMapper.java
+++ src/main/java/com/geekcattle/mapper/console/AdminMapper.java
@@ -7,8 +7,6 @@ package com.geekcattle.mapper.console;
 import com.geekcattle.model.console.Admin;
 import com.geekcattle.util.CustomerMapper;
 
-import java.util.List;
-import java.util.Optional;
 
 /**
  * author geekcattle
